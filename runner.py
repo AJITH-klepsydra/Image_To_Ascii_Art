09f9b1fa7946babb8fa76d83fc17628ef5d9dde8
Ajith-Klep <ajithpmuralidharan01@gmail.com>
2020-11-28 00:00:36 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,10 +3,10 @@ import os
 import pickle
 from main import ImageToAscii
 #asciify a video
-'''obj = ImageToAscii("/home/klepsydra/Desktop/videoplayback.mp4", 5)
-obj.video_ascii()'''
+obj = ImageToAscii("/home/klepsydra/Desktop/videoplayback.mp4", 5)
+obj.video_ascii()
 
-"""def read():
+def read():
 	pickle_off = open ("output.txt", "rb")
 	emp = pickle.load(pickle_off)
 	for el in emp:
@@ -15,7 +15,7 @@ obj.video_ascii()'''
 		os.system('clear')
 read()
 
-os.system('clear')"""
+os.system('clear')
 #asciify an image
 obj = ImageToAscii("/home/klepsydra/Pictures/me/wedding_proposal.jpg", 4)
 obj.image_ascii()
